6dbaef2cfcfea245d3dd22c72d2f3f64d50aa4b5
ArpitKumarSingh2004 <166579821+ArpitKumarSingh2004@users.noreply.github.com>
2024-09-10 12:43:16 +0530

    Add files via upload

---
+++ lab3prog1.py
@@ -0,0 +1,7 @@
+a=int(input("ENTER THE VALUE OF n: "))
+print("NUMBER       SQUARE")
+i=1
+while(i<=a):
+    print(i,"         ",i*i)
+    i=i+1
+
---
+++ lab3prog4.py
@@ -0,0 +1,11 @@
+a=int(input("ENTER THE VALUE OF n: "))
+limit=int(input("ENTER THE VALUE OF LIMIT: "))
+for i in range(1,limit+1):
+    print(a," X ",i," = ",a*i)
+
+
+
+
+65-90
+97-122
+48-57
